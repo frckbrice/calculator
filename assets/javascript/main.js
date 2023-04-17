2ec0d6afeb56b458a9a01481d726a23e1babc367
frckbrice <maebrie2017@gmail.com>
2023-04-17 13:19:44 +0100

    simple calculator fine

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,47 +2,48 @@ const keys = document.querySelectorAll(".key");
 const display_input = document.querySelector(".display .input");
 const display_output = document.querySelector(".display .output");
 
+// to get the input value(s)
 let input = "";
 
 for (let key of keys) {
   const value = key.dataset.key;
 
   key.addEventListener("click", () => {
-      if (value == "clear") {
-        input = "";
-        display_input.innerHTML = "";
-        display_output.innerHTML = "";
-      } else if (value == "backspace") {
-        input = input.slice(0, -1);
-        display_input.innerHTML = CleanInput(input);
-      } else if (value == "=") {
-        let result = eval(caseOfpercentage(input));
-
-        display_output.innerHTML = CleanOutput(result);
-      } else if (value == "brackets") {
-        if (
-          input.indexOf("(") == -1 ||
-          (input.indexOf("(") != -1 &&
-            input.indexOf(")") != -1 &&
-            input.lastIndexOf("(") < input.lastIndexOf(")"))
-        ) {
-          input += "(";
-        } else if (
-          (input.indexOf("(") != -1 && input.indexOf(")") == -1) ||
-          (input.indexOf("(") != -1 &&
-            input.indexOf(")") != -1 &&
-            input.lastIndexOf("(") > input.lastIndexOf(")"))
-        ) {
-          input += ")";
-        }
-
-        display_input.innerHTML = CleanInput(input);
-      } else {
-        if (ValidateInput(value)) {
-          input += value;
-          display_input.innerHTML = CleanInput(input);
-        }
+    if (value == "clear") {
+      input = "";
+      display_input.innerHTML = "";
+      display_output.innerHTML = "";
+    } else if (value == "backspace") {
+      input = input.slice(0, -1);
+      display_input.innerHTML = CleanInput(input);
+      display_input.innerHTML = input;
+    } else if (value == "=") {
+      let result = eval(caseOfpercentage(input));
+      display_output.innerHTML = result;
+      display_output.innerHTML = CleanOutput(result)
+    } else if (value == "brackets") {
+      if (
+        input.indexOf("(") == -1 ||
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf("(") < input.lastIndexOf(")"))
+      ) {
+        input += "(";
+      } else if (
+        (input.indexOf("(") != -1 && input.indexOf(")") == -1) ||
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf("(") > input.lastIndexOf(")"))
+      ) {
+        input += ")";
       }
+      display_input.innerHTML = CleanInput(input);
+    } else {
+      if (ValidateInput(value)) {
+        input += value;
+        display_input.innerHTML = input;
+      }
+    }
   });
 }
 
@@ -69,30 +70,36 @@ function CleanInput(input) {
       input_array[i] = `<span class="percent">%</span>`;
     }
   }
-
-  return input_array.join("").substring(0, 30);
+  // we need max 30 characters in the input string
+  return input_array.join("").substring(0, 20);
 }
 
 // a function to clean out the output field
 function CleanOutput(output) {
   let output_string = output.toString();
-  let decimal = output_string.split(".")[1];
-  output_string = output_string.split(".")[0];
+  let decimal = output_string.split(",")[1];
+  output_string = output_string.split(",")[0];
+
+  console.log(output_string);
 
   let output_array = output_string.split("");
 
-  if (output_array.length > 3) {
-    for (let i = output_array.length - 3; i > 0; i -= 3) {
-      output_array.splice(i, 0, ",");
-    }
-  }
- 
+  // if (output_array.length > 3) {
+  //   for (let i = output_array.length - 3; i > 0; i -= 3) {
+  //     if (output_array[i] == ".") {
+  //       continue;
+  //     } else {
+  //       output_array.splice(i, 0, ",");
+  //     }
+  //   }
+  // }
+
   if (decimal) {
     output_array.push(",");
     output_array.push(decimal);
   }
 
-  return output_array.join("").substring(0, 30);
+  return output_array.join("");
 }
 
 //function to validate the input
